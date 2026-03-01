a6f7bbcf1acd6fc814d933e3d170f9a369eaa128
Vishal Singh Patel <vishalpsingh638700@gmail.com>
2026-03-01 13:00:41 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,18 @@
+const classId = 1012
+// once it defines then you can never change it.
+
+var studentName = "Vishal Singh"
+// Not usable for now beacuse it not containe block scope {} and functonal scope.
+
+let fatherName = "Maansingh"
+// Todays we use this variable for define any value.
+
+console.log(classId) 
+console.log(studentName)
+console.log(fatherName);
+// console.log use to print any values.
+
+console.table([classId, studentName, fatherName]);
+// It use to print multiple values in a table forms.
+
+
