30f84c6e8ee9488dceb83fe7bec113cb497a8006
Vishal Singh Patel <vishalpsingh638700@gmail.com>
2026-03-02 23:18:20 +0530

    Create data-type.js

---
+++ data-type.js
@@ -0,0 +1,42 @@
+// Defining variable.
+
+// integer datatype
+let number = 23
+
+// string
+let name = "Vishal"
+
+// boolean datatype
+let boolean = true
+
+// null datatype
+let vishal = null
+
+// undefined datatype
+let college;
+
+// define bigint datatype.
+let bignumber = 12344567897654n
+
+// print all of the variables.
+console.log(number)
+console.log(name)
+console.log(boolean)
+console.log(vishal)
+console.log(college)
+console.log(bignumber)
+
+// print all the variables inn the table form.
+console.table([number, name, boolean, vishal, college, bignumber])
+
+// print type of all the variables.
+console.log(typeof number)
+console.log(typeof name)
+console.log(typeof boolean)
+console.log(typeof vishal)
+console.log(typeof college)
+console.log(typeof bignumber)
+
+// print type of all variables in a table form.
+console.table([typeof number, typeof name,typeof boolean, typeof vishal, typeof bignumber, typeof college]);
+
