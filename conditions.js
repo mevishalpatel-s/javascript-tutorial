d8a70bc2e981cef4b0c2ae60a289e9c032e627ff
Vishal Singh Patel <vishalpsingh638700@gmail.com>
2026-03-06 21:19:49 +0530

    Update conditions.js

--- conditions.js
+++ conditions.js
@@ -2,52 +2,51 @@
 
 // ================================ If statement =============================================================
 
-// let age = 20;
+let age = 20;
 
-// if(age > 18){
-//     console.log("You can vote!")
-// }
+if(age > 18){
+    console.log("You can vote!")
+}
 
-// if(age < 18){
-//     console.log("You CANNOT vote!")
-// }
+if(age < 18){
+    console.log("You CANNOT vote!")
+}
 
 // =============================== If-else Statement ============================================================
 
 
-// if(age > 18){
-//     console.log("You can vote!")
-// }
-// else{
-//     console.log("You CANNOT vote!")
-// }
+if(age > 18){
+    console.log("You can vote!")
+}
+else{
+    console.log("You CANNOT vote!")
+}
 
 // =============================== else-if statement ===============================================================
 
-// let marks = 61;
+let marks = 61;
 
-// if (marks>90){
-//     console.log("Grade A : Pass");
-// }
+if (marks>90){
+    console.log("Grade A : Pass");
+}
 
-// else if(marks>=80){
-//     console.log("Grade B : Pass");
-// }
-// else if(marks>=70){
-//     console.log("Grade C : Pass");
-// }
-// else if(marks>60){
-//     console.log("Grade D : Pass");
-// }
-// else{
-//     console.log("Grade F, Try next time again!");
-// }
+else if(marks>=80){
+    console.log("Grade B : Pass");
+}
+else if(marks>=70){
+    console.log("Grade C : Pass");
+}
+else if(marks>60){
+    console.log("Grade D : Pass");
+}
+else{
+    console.log("Grade F, Try next time again!");
+}
 
 // ================================= Ternary Operators =================================================================
 
 //Syntax: condition ? true output : false output
 
-let age = 20;
 let user = age > 18 ? "Adult" : "NOt Adult"
 console.log(user)
 
