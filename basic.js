2f307d2904db6acb87c6e7b5945f4681ba9fc3d5
Vishal Singh Patel <vishalpsingh638700@gmail.com>
2026-03-06 21:32:41 +0530

    Update basic.js

--- basic.js
+++ basic.js
@@ -3,11 +3,37 @@
 console.log("Hello World!")
 
 
-// =============================== Comments ====================================
+// =============================== Comments ==============================================================
 
 //  we use "//" for single comment.  Comments are part of programs but not for the executions.
-// THis is a single line comments.
+
+// This is a single line comments.
 
 /* This is a multiline comment
 use to decribe code in javaScipt.*/
 
+
+
+// ============================ Alert: One time PopUp =====================================================================
+
+alert("Hello World! ")   // Only for browser.
+
+
+// ============================= prompt: Use to take the input from the User. ==============================================
+
+let name = prompt("Enter Your name: ")
+console.log(name);
+
+
+// Example:
+
+let num = prompt("Enter a number: ")
+
+if (num % 5 === 0){
+    console.log(num, "is a multiple of 5");
+}
+else{
+    console.log(num, "is not a multiple of 5");    
+}
+
+console.log(num)
