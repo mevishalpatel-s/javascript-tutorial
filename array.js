9c40aa27127e33b06734a14a3ebf06ee380797bf
Vishal Singh Patel <vishalpsingh638700@gmail.com>
2026-03-07 23:51:29 +0530

    Create array.js

---
+++ array.js
@@ -0,0 +1,48 @@
+//=========================================== Array: collection of items.====================================================
+
+// let array = [1,2,3,4,5,6]
+// console.log(array);
+
+// to store the marks of students.
+
+// let marks = [98,90,80,65,78]
+// console.log(marks)
+// console.log(marks.length)
+// console.log(typeof marks);
+
+// Students name.
+
+// let student = ["Vishal", "Aman", "Priya", "Ahmed"]
+// console.log(student)
+// console.log(student.length)
+
+//========================== Array Indices/index ==========================================================
+
+// let arr = [12, 34, 56, 78, 98]
+// console.log(arr)
+// console.log(arr[3]);
+
+// ========================= to change any value of array =================================================
+
+// let b = [23, 40, 34, 59, 34, 54]
+// console.log(b)
+// console.log(b[3])
+// console.log(b[3] = 23);  // this syntax use for change the value.
+// console.log(b)
+
+//====================== Looping on a Array =================================================================
+// use to print all elements of Array.
+
+//for loop =========================================================================
+// let c = [23, 40, 34, 59, 34, 54]
+// for (let i = 0; i < c.length; i++){
+//     console.log(c[i])
+// }
+
+//for-of loop =======================================================================
+
+let c = [23, 40, 34, 59, 34, 54]
+for (let i of c){
+    console.log(i)
+}
+
