2b49e64e4e454e80ab35369c11da68b093e113f2
Vishal Singh Patel <vishalpsingh638700@gmail.com>
2026-03-11 18:28:22 +0530

    Update array.js

--- array.js
+++ array.js
@@ -1,55 +1,54 @@
 //=========================================== Array: collection of items.====================================================
 
-// let array = [1,2,3,4,5,6]
-// console.log(array);
+let array1 = [1,2,3,4,5,6]
+console.log(array1);
 
 // to store the marks of students.
 
-// let marks = [98,90,80,65,78]
-// console.log(marks)
-// console.log(marks.length)
-// console.log(typeof marks);
+let marks = [98,90,80,65,78]
+console.log(marks)
+console.log(marks.length)
+console.log(typeof marks);
 
 // Students name.
 
-// let student = ["Vishal", "Aman", "Priya", "Ahmed"]
-// console.log(student)
-// console.log(student.length)
+let student = ["Vishal", "Aman", "Priya", "Ahmed"]
+console.log(student)
+console.log(student.length)
 
 //========================== Array Indices/index ==========================================================
 
-// let arr = [12, 34, 56, 78, 98]
-// console.log(arr)
-// console.log(arr[3]);
+let array = [12, 34, 56, 78, 98]
+console.log(array)
+console.log(array[3]);
 
 // ========================= to change any value of array =================================================
 
-// let b = [23, 40, 34, 59, 34, 54]
-// console.log(b)
-// console.log(b[3])
-// console.log(b[3] = 23);  // this syntax use for change the value.
-// console.log(b)
+let b = [23, 40, 34, 59, 34, 54]
+console.log(b)
+console.log(b[3])
+console.log(b[3] = 23);  // this syntax use for change the value.
+console.log(b)
 
 //====================== Looping on a Array =================================================================
 // use to print all elements of Array.
 
 //for loop =========================================================================
-// let c = [23, 40, 34, 59, 34, 54]
-// for (let i = 0; i < c.length; i++){
-//     console.log(c[i])
-// }
+let c = [23, 40, 34, 59, 34, 54]
+for (let i = 0; i < c.length; i++){
+    console.log(c[i])
+}
 
 //for-of loop =======================================================================
 
-// let c = [23, 40, 34, 59, 34, 54]
-// for (let i of c){
-//     console.log(i)
-// }
+for (let i of c){
+    console.log(i)
+}
 
-// let cities = ["Delhi", "Goa", "Mumbai", "Lucknow"]
-// for (city of cities){
-//     console.log(city)
-// }
+let cities = ["Delhi", "Goa", "Mumbai", "Lucknow"]
+for (city of cities){
+    console.log(city)
+}
 
 // ================================ Araay methodes ============================================================================
 
@@ -72,8 +71,42 @@ console.log(fruits.toString)
 
 // =================== 4. toConcat()  --- Use to add arrays and get another a
 
+let city = ["Lucknow", "Delhi", "Kanpur", "Noida"]
+let country = ["India", "Pakistan", "China", "Afganistan"]
+
+let newArr = city.concat(country)   //---use to concat the arrays.
 
 
+// ================= 5. unShift() // --- use to add elements in start of the array.
+
+fruits.unshift("Lichi")
 
 
+// ================= 6. shift()  // --- use to remove element from the start.
 
+fruits.shift()
+
+
+// ================ 7. slice(startIndex, endIndex)  // --- return a piece of array.
+
+fruits.slice(0, 2)
+
+
+// =============== 8. splice(startIndex, delcount, newElement1, newElement2...)
+
+
+let arr = [1, 2, 3, 4, 5, 6, 7]
+
+arr.splice(2, 3, 100, 102)  // -- by usung this syntex we delete 3 items after 2 index means 4,5 and 6. And add two element 100, 101 on the 3rd index and 4th index.
+
+// to add element.
+arr.splice(2, 0, 100)   // -- 0 deletion after 2nd index and add an element after 2nd index.
+
+// to delete items.
+arr.splice(2, 3)    // --- delete three items after 2nd index
+
+// to replace any item.
+arr.splice(2, 1, 105)  // -- delete 3 index and replace it to 105.
+
+//delete a piece of array.
+arr.splice(4)  // --- after 4 index all the items will deleted.
